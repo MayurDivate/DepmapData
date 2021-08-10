c916d8e14fd08e2d359a0e23ff087da424f91762
Mayur Divate <mayur.divate91@gmail.com>
2021-08-10 18:31:56 +1000

    beta version

---
+++ depmap_scraper.py
@@ -0,0 +1,48 @@
+import requests
+import pandas as pd
+import json
+
+
+
+def download_latest_sample_info():
+    url = 'https://ndownloader.figshare.com/files/27902376'
+    response = requests.get(url, allow_redirects=True)
+    open('sample_info.csv', 'wb').write(response.content)
+
+class DepmapScraper():
+
+    def __init__(self, query):
+        self.query = query
+        self.depmap_url = 'https://depmap.org/portal/partials/data_table/context_dependency_enrichment?context='+query
+
+    def get_data_table(self):
+        response = requests.get(self.depmap_url).text
+        js = json.loads(response)
+
+        df = pd.DataFrame.from_dict({ i : d for i, d in enumerate(js['data'])}, orient='index')
+        df.columns = js['cols']
+        df['type'] = self.query
+        return df
+
+
+def download_dependency_enrichement_data(by_type='lineage_subtype'):
+
+    # dwonload sample info file
+    #download_latest_sample_info()
+    data = []
+
+    samples = pd.read_csv('sample_info.csv')
+
+    for typeX in samples[by_type].unique():
+        df = DepmapScraper(typeX).get_data_table()
+        data.append(df)
+
+    data = pd.concat(data)
+
+    out = by_type+'_dependency_enrichment.csv'
+
+    data.to_csv(out)
+
+download_dependency_enrichement_data()
+
+
